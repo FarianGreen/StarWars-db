ccefdf94a0c933e3a45197e485d2f89317cf4ae7
FarianGreen <aremfarafonov6@gmail.com>
2023-02-07 19:56:38 +0300

    update app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
               <Header />
               <RandomPlanet />
               <Routes>
-                <Route path="/" element={<StartPage />} />
+                <Route path="/" exact element={<StartPage />} />
                 <Route path="people" element={<PeoplePage />} />
                 <Route path="planets" element={<PlanetsPage />} />
                 <Route path="starships" element={<StarshipsPage />} />
